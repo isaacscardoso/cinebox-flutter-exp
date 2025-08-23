0de6b3c14fd3d144c1c3e53269fe98639c66e54d
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-23 19:37:50 -0300

    Adds the application's textual style class.

---
+++ lib/ui/core/themes/app_text_styles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+abstract final class AppTextStyles {
+  static const titleLarge = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const subtitleSmall = TextStyle(
+    fontSize: 14,
+    color: AppColors.lightGrey,
+  );
+
+  static const boldSmall = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const boldMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const boldLarge = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const semiBoldMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const regularSmall = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const lightGreyRegular = TextStyle(
+    fontSize: 14,
+    color: AppColors.lightGrey,
+  );
+
+  static const lightGreySmall = TextStyle(
+    fontSize: 12,
+    color: AppColors.lightGrey,
+  );
+
+  static const errorText = TextStyle(
+    color: AppColors.red,
+  );
+}
