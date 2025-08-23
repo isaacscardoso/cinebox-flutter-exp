4e9bea5117aab6f70d740c5eefae0eae2455a24b
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-23 19:37:32 -0300

    Adds the application color class.

---
+++ lib/ui/core/themes/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const red = Color(0xFFEA4335);
+  static const yellow = Color(0xFFFFBA49);
+  static const darkGrey = Color(0xFF4F4F4F);
+  static const lightGrey = Color(0xFF9B9B9B);
+  static const background = Color(0xFFF9F9F9);
+}
