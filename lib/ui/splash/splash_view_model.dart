d700a23bc564786edbb45565e2b2e78870de66b5
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-24 16:28:25 -0300

    feat: Adds the splash view model to listen to changes in the logged in user check and decide what to do.

---
+++ lib/ui/splash/splash_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'commands/check_user_logged_command.dart';
+
+part 'splash_view_model.g.dart';
+
+final class SplashViewModel {
+  final CheckUserLoggedCommand _checkUserLoggedCommand;
+
+  SplashViewModel({
+    required CheckUserLoggedCommand checkUserLoggedCommand,
+  }) : _checkUserLoggedCommand = checkUserLoggedCommand;
+
+  void checkLoginAndRedirect() {
+    _checkUserLoggedCommand.execute();
+  }
+}
+
+@riverpod
+SplashViewModel splashViewModel(Ref ref) {
+  return SplashViewModel(
+    checkUserLoggedCommand: ref.read(checkUserLoggedCommandProvider.notifier),
+  );
+}
