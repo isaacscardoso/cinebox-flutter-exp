f180f0192d6d86505e6abdc84034f2593efc6557
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-24 16:29:27 -0300

    feat(SplashScreen): Adds logged in user verification and redirection logic.

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/themes/resource.dart';
 import '../core/widgets/loader.dart';
 import '../core/widgets/snack_bar_messages.dart';
+import 'commands/check_user_logged_command.dart';
+import 'splash_view_model.dart';
 
 final class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +17,44 @@ final class SplashScreen extends ConsumerStatefulWidget {
 final class _SplashScreenState extends ConsumerState<SplashScreen>
     with Loader, SnackBarMessages {
   @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(splashViewModelProvider).checkLoginAndRedirect();
+    });
+    super.initState();
+  }
+
+  void _redirectTo(String path) {
+    Navigator.pushNamedAndRemoveUntil(context, path, (route) => false);
+  }
+
+  void _checkLoginAndRedirectListener() {
+    ref.listen(checkUserLoggedCommandProvider, (_, next) {
+      next.whenOrNull(
+        data: (data) {
+          final path = switch (data) {
+            true => '/home',
+            false => '/login',
+            _ => '',
+          };
+          if (path.isNotEmpty && context.mounted) {
+            _redirectTo(path);
+          }
+        },
+        error: (error, stackTrace) {
+          if (context.mounted) {
+            showErrorSnackBar('Erro ao verificar login.');
+            _redirectTo('/login');
+          }
+        },
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _checkLoginAndRedirectListener();
+
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
