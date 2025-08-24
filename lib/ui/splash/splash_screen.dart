61fb3a4eb0f35f350b661f501fbe77913c8c9cd4
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-24 15:42:29 -0300

    feat(SplashScreen): Adds the SplashScreen widget.

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../core/themes/resource.dart';
+import '../core/widgets/loader.dart';
+import '../core/widgets/snack_bar_messages.dart';
+
 final class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
@@ -8,14 +12,29 @@ final class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState createState() => _SplashScreenState();
 }
 
-final class _SplashScreenState extends ConsumerState<SplashScreen> {
+final class _SplashScreenState extends ConsumerState<SplashScreen>
+    with Loader, SnackBarMessages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Screen'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            fit: BoxFit.cover,
+          ),
+          const ColoredBox(
+            color: Color.fromARGB(170, 0, 0, 0),
+          ),
+          Center(
+            child: Image.asset(
+              R.ASSETS_IMAGES_LOGO_PNG,
+              excludeFromSemantics: true,
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
