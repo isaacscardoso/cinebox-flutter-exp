e552526b0a38e26d98a75197905bfbfba84ef960
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-25 17:33:28 -0300

    feat: Adds backend authentication call.

--- lib/data/repositories/auth/auth_repository_impl.dart
+++ lib/data/repositories/auth/auth_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
+
 import '../../../core/app/app_messages.dart';
 import '../../../core/result/result.dart';
 import '../../exceptions/data_exception.dart';
+import '../../services/auth/auth_service.dart';
 import '../../services/google_signin/google_signin.dart';
 import '../../services/local_storage/local_storage.dart';
 import 'auth_repository.dart';
@@ -10,18 +13,27 @@ import 'auth_repository.dart';
 final class AuthRepositoryImpl implements AuthRepository {
   final LocalStorageService _localStorageService;
   final GoogleSignInService _googleSignInService;
+  final AuthService _authService;
 
   const AuthRepositoryImpl({
     required LocalStorageService localStorageService,
     required GoogleSignInService googleSignInService,
+    required AuthService authService,
   }) : _localStorageService = localStorageService,
-       _googleSignInService = googleSignInService;
+       _googleSignInService = googleSignInService,
+       _authService = authService;
 
   Result<T> _registerLogAndReturnFailure<T>(
     String message,
-    Exception error,
-  ) {
-    log(message, name: 'AuthRepositoryImpl', error: error);
+    Exception error, [
+    StackTrace? stackTrace,
+  ]) {
+    log(
+      message,
+      name: 'AuthRepositoryImpl',
+      error: error,
+      stackTrace: stackTrace,
+    );
     return Failure(DataException(message: message));
   }
 
@@ -31,9 +43,17 @@ final class AuthRepositoryImpl implements AuthRepository {
 
     switch (result) {
       case Success<String>(:final value):
-        await _localStorageService.saveIdToken(value);
-        // TODO: Back-end Login
-        return successOfUnit();
+        try {
+          await _localStorageService.saveIdToken(value);
+          await _authService.auth();
+          return successOfUnit();
+        } on DioException catch (error, stackTrace) {
+          return _registerLogAndReturnFailure(
+            AppMessages.backendAuthError,
+            error,
+            stackTrace,
+          );
+        }
       case Failure<String>(:final error):
         return _registerLogAndReturnFailure(AppMessages.loginError, error);
     }
