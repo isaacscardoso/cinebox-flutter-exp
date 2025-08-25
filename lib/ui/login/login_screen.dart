39d712428da87a266eafc22a14ce31a9f379ddc8
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-25 13:39:39 -0300

    feat: Develops the style of the login screen.

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../core/themes/resource.dart';
+import 'widgets/sign_in_google_button.dart';
+
 final class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
 
@@ -11,9 +14,34 @@ final class LoginScreen extends ConsumerStatefulWidget {
 final class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            fit: BoxFit.cover,
+          ),
+          const ColoredBox(
+            color: Color.fromARGB(170, 0, 0, 0),
+          ),
+          Column(
+            spacing: 48,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 108),
+                child: Image.asset(
+                  R.ASSETS_IMAGES_LOGO_PNG,
+                  excludeFromSemantics: true,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: SignInGoogleButton(),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
