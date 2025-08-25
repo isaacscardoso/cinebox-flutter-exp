13be3f4d158ef0f8ae8a4f289762edb827c4bd35
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-25 15:28:36 -0300

    feat: Adjusts the 'SignInGoogleButton' for login functionality.

--- lib/ui/login/widgets/sign_in_google_button.dart
+++ lib/ui/login/widgets/sign_in_google_button.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import '../../core/themes/app_text_styles.dart';
 import '../../core/themes/resource.dart';
 
 final class SignInGoogleButton extends StatelessWidget {
-  const SignInGoogleButton({super.key});
+  final VoidCallback onPressed;
+  final bool isLoading;
+
+  const SignInGoogleButton({
+    super.key,
+    required this.onPressed,
+    required this.isLoading,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,20 +21,32 @@ final class SignInGoogleButton extends StatelessWidget {
         backgroundColor: Colors.white,
         padding: EdgeInsets.zero,
       ),
-      onPressed: () {},
+      onPressed: onPressed,
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
           Align(
             alignment: Alignment.centerLeft,
-            child: Image.asset(
-              R.ASSETS_IMAGES_GOOGLE_LOGO_PNG,
-              excludeFromSemantics: true,
+            child: Visibility(
+              visible: !isLoading,
+              replacement: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: LoadingAnimationWidget.threeArchedCircle(
+                  color: Colors.black,
+                  size: 20,
+                ),
+              ),
+              child: Image.asset(
+                R.ASSETS_IMAGES_GOOGLE_LOGO_PNG,
+                excludeFromSemantics: true,
+              ),
             ),
           ),
-          const Text(
+          Text(
             'Entrar com o Google',
-            style: AppTextStyles.darkGreyRegular,
+            style: !isLoading
+                ? AppTextStyles.darkGreyRegular
+                : AppTextStyles.lightGreyRegular,
           ),
         ],
       ),
