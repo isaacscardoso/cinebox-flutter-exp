b8c9a19dc37c6b34154deba8392450a9f64641d7
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-25 00:31:44 -0300

    feat: Adds the 'AuthRepository' contract interface.

---
+++ lib/data/repositories/auth/auth_repository.dart
@@ -0,0 +1,9 @@
+import '../../../core/result/result.dart';
+
+abstract interface class AuthRepository {
+  Future<Result<Unit>> signIn();
+
+  Future<Result<Unit>> signOut();
+
+  Future<Result<bool>> isSignedIn();
+}
