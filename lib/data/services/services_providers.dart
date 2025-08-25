af40cba16dab8003ed7a1b2585c50d6f9692e579
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-24 22:35:09 -0300

    feat: Adds the 'GoogleSignInService' service to the Riverpod services providers.

--- lib/data/services/services_providers.dart
+++ lib/data/services/services_providers.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import 'local_storage/local_storage_service.dart';
-import 'local_storage/local_storage_service_impl.dart';
+import 'google_signin/google_signin.dart';
+import 'local_storage/local_storage.dart';
 
 part 'services_providers.g.dart';
+
+@riverpod
+GoogleSignInService googleSignInService(Ref ref) {
+  return GoogleSignInServiceImpl();
+}
 
 @Riverpod(keepAlive: true)
 LocalStorageService localStorageService(Ref ref) {
