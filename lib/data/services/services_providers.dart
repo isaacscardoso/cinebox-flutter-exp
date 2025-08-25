0102a7f657b4bef20c478ef848a3e4fc8ac9b92f
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-25 17:33:03 -0300

    feat: Adds riverpod service provider for 'authService'.

--- lib/data/services/services_providers.dart
+++ lib/data/services/services_providers.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../rest_client/backend_rest_client_provider.dart';
+import 'auth/auth_service.dart';
 import 'google_signin/google_signin.dart';
 import 'local_storage/local_storage.dart';
 
@@ -16,4 +18,9 @@ LocalStorageService localStorageService(Ref ref) {
   return const LocalStorageServiceImpl(
     flutterSecureStorage: FlutterSecureStorage(),
   );
+}
+
+@riverpod
+AuthService authService(Ref ref) {
+  return AuthService(ref.read(backendRestClientProvider));
 }
