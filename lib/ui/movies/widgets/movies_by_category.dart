fa85d3c62347be1ac6394ac2a76bd2112b281963
isaacscardoso <isaacscardoso@protonmail.com>
2025-08-26 15:22:37 -0300

    feat: Adds titles widget by movie section by category.

---
+++ lib/ui/movies/widgets/movies_by_category.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'movies_box.dart';
+
+final class MoviesByCategory extends ConsumerStatefulWidget {
+  const MoviesByCategory({super.key});
+
+  @override
+  ConsumerState createState() => _MoviesByCategoryState();
+}
+
+final class _MoviesByCategoryState extends ConsumerState<MoviesByCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: const Column(
+        children: <Widget>[
+          MoviesBox(title: 'Mais Populares'),
+          MoviesBox(title: 'Top Filmes'),
+        ],
+      ),
+    );
+  }
+}
